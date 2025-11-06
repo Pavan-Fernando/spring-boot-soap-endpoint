00ef04ba6ee33926b6bf4f7a37190375d9836cdf
Pavan Fernando <pavanfernando6@gmail.com>
2025-11-06 11:18:19 +0530

    init

---
+++ src/main/java/com/example/soapendpoint/SoapendpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapendpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapendpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soapendpoint/SoapendpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapendpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
